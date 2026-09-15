988c58382bdfc3676bd6e4fa7d62cf234f835a10
JJTTZZ-0816 <heidy25@163.com>
2026-09-16 02:27:25 +0800

    Add dice rolling and scoring functionality
    Implement dice rolling logic and scoring in hog.py.

--- task2_lab/python_lab/hog/hog.py
+++ task2_lab/python_lab/hog/hog.py
@@ -21,7 +21,17 @@ def roll_dice(num_rolls, dice=six_sided):
     assert type(num_rolls) == int, "num_rolls must be an integer."
     assert num_rolls > 0, "Must roll at least once."
     # BEGIN PROBLEM 1
-    "*** YOUR CODE HERE ***"
+    total = 0
+    special = False
+    for _ in range(num_rolls):
+        point=dice()
+        total += point
+        if point == 1:
+           special = True
+    if special == True:
+       return 1
+    else ：
+       return total
     # END PROBLEM 1
 
 
@@ -33,7 +43,9 @@ def boar_brawl(player_score, opponent_score):
 
     """
     # BEGIN PROBLEM 2
-    "*** YOUR CODE HERE ***"
+    g1 = player_score % 10
+    g2 = (opponent_score // 10) % 10
+    return max(3*abs(g1-g2),1)
     # END PROBLEM 2
 
 
@@ -51,7 +63,10 @@ def take_turn(num_rolls, player_score, opponent_score, dice=six_sided):
     assert num_rolls >= 0, "Cannot roll a negative number of dice in take_turn."
     assert num_rolls <= 10, "Cannot roll more than 10 dice."
     # BEGIN PROBLEM 3
-    "*** YOUR CODE HERE ***"
+    if num_rolls == 0:
+      return boar_brawl(player_score, opponent_score
+    else:
+        return roll_dice(num_rolls, dice)
     # END PROBLEM 3
 
 
